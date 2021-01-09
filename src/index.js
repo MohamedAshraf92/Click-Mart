c7bbd3de47b96e89b6e9d03649d774adb77f6bcb
MohamedAshraf92 <eng.mohamed.hfs@gmail.com>
2021-01-09 16:24:35 +0200

    FireBase deploy

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore } from 'redux'
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './store/reducers/rootReducer'
-
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const saveToLocalStorage = (state) => {
   try {
@@ -39,7 +36,7 @@ const persistedState = loadFromLocalStorage()
 const store = createStore(
   rootReducer,
   persistedState,
-  composeEnhancers(applyMiddleware(thunk))
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 store.subscribe(() => saveToLocalStorage(store.getState()))
